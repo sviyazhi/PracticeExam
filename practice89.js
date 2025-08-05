ed64634f01ee41171cf1482a894c83036623ef27
sviyazhi <bit22-sviyazhi@mubas.ac.mw>
2025-08-05 13:17:00 +0200

    final practice draft

--- practice89.js
+++ practice89.js
@@ -114,3 +114,67 @@ function calculateHypotenuse(adjacent, opposite) {
 let hypotenuse = calculateHypotenuse(3, 4);
 console.log("Hypotenuse:", hypotenuse);
 
+//Q9
+function vowelCount(sentence) {
+    let matches = sentence.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
+}
+
+console.log(vowelCount("Hello World")); 
+
+//Q10a
+
+const post = {
+  postId: "123456789",
+  author: {
+    userId: "ugyy7gh7",
+    name: "John Doe",
+    tags: ["Alice Wawukali", "James Wantali", "Peter Woyera"]
+}
+};
+
+class Post {
+  constructor(postId, author) {
+    this.postId = postId;
+    this.author = author;
+  }
+}
+
+const post2 = new Post("123456789", { name: "John Doe" });
+
+function PostConstructor(postId, author) {
+  this.postId = postId;
+  this.author = author;
+}
+
+const post3 = new PostConstructor("123456789", { name: "John Doe" });
+
+
+
+
+
+
+//Q10c
+
+const jsonString = JSON.stringify(post);
+console.log(jsonString);
+
+const jsObject = JSON.parse(jsonString);
+console.log(jsObject);
+
+//Q11
+function countVowels(sentence) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    const result = { a: 0, e: 0, i: 0, o: 0, u: 0 };
+    for (let char of sentence.toLowerCase()) {
+        if (vowels.includes(char)) {
+            result[char]++;
+        }
+    }
+    return result;
+}
+
+console.log(countVowels("Beautiful day at the zoo"));
+
+
+
